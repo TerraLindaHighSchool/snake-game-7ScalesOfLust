a50d86c6d8924fa498cf7790395ab0d585f20d96
7ScalesOfLUst <coolcatsteelst@gmail.com>
2019-01-16 10:34:15 -0800

    Lab 1 is complete and ready for grading.

--- app/src/main/java/com/example/bruce/snake_startercode/SnakeSegment.java
+++ app/src/main/java/com/example/bruce/snake_startercode/SnakeSegment.java
@@ -1,4 +1,49 @@
 package com.example.bruce.snake_startercode;
 
 public class SnakeSegment {
+
+    private int mXLoc;
+    private int mYLoc;
+    private int mDegrees;
+    private bodyPart mBodyPart;
+
+    SnakeSegment(int xLoc, int yLoc, int degrees, bodyPart bodyPart){
+        mXLoc = xLoc;
+        mYLoc = yLoc;
+        mDegrees = degrees;
+        mBodyPart = bodyPart;
+    }
+
+    public enum bodyPart{
+        HEAD, BODY, TAIL
+    }
+
+    public bodyPart getBodyParts() {
+        return mBodyPart;
+    }
+
+    public int getXLoc() {
+        return mXLoc;
+    }
+
+    public void setXLoc(int XLoc) {
+        mXLoc = XLoc;
+    }
+
+    public int getYLoc() {
+        return mYLoc;
+    }
+
+    public void setYLoc(int YLoc) {
+        mYLoc = YLoc;
+    }
+
+    public int getDegrees() {
+        return mDegrees;
+    }
+
+    public void setDegrees(int degrees) {
+        mDegrees = degrees;
+    }
+
 }
---
+++ app/src/test/java/com/example/bruce/snake_startercode/SnakeSegmentTest.java
@@ -0,0 +1,37 @@
+package com.example.bruce.snake_startercode;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SnakeSegmentTest {
+
+    @Test
+    public void getBodyParts() {
+    }
+
+    @Test
+    public void getXLoc() {
+    }
+
+    @Test
+    public void setXLoc() {
+    }
+
+    @Test
+    public void getYLoc() {
+    }
+
+    @Test
+    public void setYLoc() {
+    }
+
+    @Test
+    public void getDegrees() {
+    }
+
+    @Test
+    public void setDegrees() {
+    }
+}
